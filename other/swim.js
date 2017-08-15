150163b7ec3f194ae7c8efaaadea87a6e35dfb67
Matt <Matthewbarlow98@gmail.com>
2017-08-15 23:34:36 +0100

    size changes now

--- other/swim.js
+++ other/swim.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
     
     var wetclass= ".splashzone";
-    var maxwater = 200;
+    var maxwater = 75;
     function moisture() {
         if($('.water').length <= maxwater){
-            $(wetclass).append('<div style="padding-bottom: '+Math.random()*400+'px; opacity: '+Math.random()+';"class="water">🌊</div>');
+            $(wetclass).append('<div style="padding-bottom: '+Math.random()*400+'px; opacity: '+Math.random()+'; font-size: '+Math.random()*6+'rem;"class="water">🌊</div>');
             setTimeout(function(){ moisture(); }, Math.random()*5000);
         }
-
-        
     }
     moisture();
 });
