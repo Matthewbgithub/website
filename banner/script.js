27657cc1365c9234128a85d80b6a65ac88bd270d
Matt <Matthewbarlow98@gmail.com>
2017-10-06 00:12:53 +0100

    it works nice

--- banner/script.js
+++ banner/script.js
@@ -1,38 +1,57 @@
 $(document).ready(function () {
 
     var lastScrollTop = 0;
-    var scrollDownPos = 0;
-    var scrollUpPos = 0;
+    var scrollToPos = 0;
     var isScrollingDown = true;
-	var se = '.ScrollBanner';
-	$(window).scroll(function (event) {
+    var isBetweenScrolling = false;
+    var se = '.ScrollBanner';
+    //    var bannerHeight = $(se).height();
+    var bannerHeight = 40;
+    $(window).scroll(function (event) {
 
-		var st = $(window).scrollTop();
-		if (st > lastScrollTop) {
-//            scrolling down
-            if(!isScrollingDown){
-                scrollDownPos = st;
+        var st = $(window).scrollTop();
+        //        console.log(isBetweenScrolling + " - to: " + scrollToPos + ", at: " + st);
+        if (st > lastScrollTop) {
+            //            scrolling down
+            if (!isScrollingDown) {
+
+                if (!isBetweenScrolling) {
+                    scrollToPos = st;
+                }
             }
+            isBetweenScrolling = true;
             isScrollingDown = true;
-			$(se).css('position','absolute');
-            $(se).css('transform','translateY('+scrollDownPos+'px)')
-            
-		} else {
-//            scrolling up
-            if(isScrollingDown){
-                scrollUpPos = st;
-                scrollUpPos -= $(se).height();
+            $(se).css('position', 'absolute');
+            $(se).css('transform', 'translateY(' + scrollToPos + 'px)');
+
+            if (st > (scrollToPos + bannerHeight)) {
+                //when scrolling down finshes
+                isBetweenScrolling = false;
+                $(se).css('transform', 'translateY(-' + bannerHeight + 'px)')
+                $(se).css('position', 'fixed');
             }
+        } else {
+            //            scrolling up
+
+            if (isScrollingDown) {
+                if (!isBetweenScrolling) {
+                    scrollToPos = st;
+                    scrollToPos -= bannerHeight;
+                }
+                $(se).css('position', 'absolute');
+            }
+            isBetweenScrolling = true;
             isScrollingDown = false;
-            $(se).css('transform','translateY('+scrollUpPos+'px)')
-            if(st < scrollUpPos){
-                $(se).css('transform','translateY(0px)')
-                $(se).css('position','fixed');
+            $(se).css('transform', 'translateY(' + scrollToPos + 'px)');
+
+            if (st < scrollToPos) {
+                isBetweenScrolling = false;
+                $(se).css('transform', 'translateY(0px)')
+                $(se).css('position', 'fixed');
             }
-            
-		}
-		lastScrollTop = st;
-	});
+        }
+        lastScrollTop = st;
+    });
 
 
 });
